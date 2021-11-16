9ba97063024f66bbab9936c499b567791b5311bb
Mofya Ndabala <mofyan39@gmail.com>
2021-11-16 22:25:38 +0200

    initial release

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,15 +20,9 @@ function Footer () {
             font-bold 
             font-mono
             text-center 
-            p-4 
-            lg:text-2xl sm:text-lg md:text-xl
+            p-4
             shadow-sm
              ">
-              <div className="my-5 py-2 font-bold relative">
-                <a className="pr-4 hover:text-gray-900" href="/" >Home</a>
-                <a className="pr-4 hover:text-gray-900" href="/About">About</a>
-                <a className="pr-4 hover:text-gray-900" href="/Contact">Contact</a>
-            </div>
             &copy; Mofya Ndabala <span>{ getYear() }</span> Production.
              
               </footer>
